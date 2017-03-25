3cfc5e2bad4b8b2d6decf2255b581d36144211a7
Elias Ojala <elias.jh.ojala@gmail.com>
2017-03-26 00:50:58 +0200

    PhotoSwipe Cleanup

--- PhotoSwipe/demo.php
+++ PhotoSwipe/demo.php
@@ -1,30 +1,17 @@
 <?php include 'begin.php' ?>
-
-<head>
+<html>
+  <head>
     <meta charset="UTF-8">
     <link rel='stylesheet prefetch' href='photoSwipe.css'>
   </head>
-
   <body>
-
     <div id="images">
-
-     <?php
-
-        $dirname = "../media/";
-        $images = glob($dirname."*.jpg");
-
-        showImages($images);
-     ?>
-
-
+       <?php
+          $dirname = "../media/";
+          $images = glob($dirname."*.jpg");
+          showImages($images);
+       ?>
     </div>
-
     <?php include 'end.php' ?>
-
-
-
-
-
   </body>
 </html>
