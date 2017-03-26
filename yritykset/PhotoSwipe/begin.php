7fdce07b06f28fef72fc743f97083e6e98c8c400
Elias Ojala <elias.jh.ojala@gmail.com>
2017-03-26 11:33:12 +0300

    Updates to yritykset images

--- yritykset/PhotoSwipe/begin.php
+++ yritykset/PhotoSwipe/begin.php
@@ -1,9 +1,13 @@
 <?php
   function showImage($image) {
 
-    $desc = file_get_contents($image.".txt");
+    $desc = "";
     $description = "<figcaption>".$desc."</figcaption>";
     list($width, $height) = getimagesize($image);
+    if($width < 500 && $height < 500) {
+      $width = $width * 500;
+      $height = $height * 500;
+    }
     $imgSize = $width . "x" . $height;
 
 ?>
@@ -11,7 +15,6 @@
       <figure class="imageBlock">
         <a href="<?php echo $image; ?>" data-size="<?php echo $imgSize; ?>">
             <img src="<?php echo $image; ?>"><br>
-            <span class="desc"><?php echo substr($desc, 0, 20); ?>...</span>
         </a>
         <?php echo $description; ?>
       </figure>
@@ -26,7 +29,7 @@
     <?php
       if(is_array($images)) {
         foreach ($images as $image) {
-          showImage($image);
+          if($image != "") { showImage($image); }
         }
       }
       else {
--- yritykset/sitecontent.php
+++ yritykset/sitecontent.php
@@ -1,75 +1,75 @@
 <?php
 
-  $imageList = "
-  assets/images/placeHolder_2.JPG
-  assets/images/placeHolder_2.JPG
-  ";
+$imageList = "
+assets/images/placeHolder_2.JPG
+assets/images/placeHolder_2.JPG
+";
 
-  $bulletList = "
-  Pienikokoinen, 66x180x23 (LxKxS cm)
-  Hyväksyy kaikki eurokolikot, 5snt - 2€
-  Mobiili- ja luottokorttimaksu
-  Helppo asennus ja käyttöönotto
-  Vapaa hinnoittelu
-  Etävalvonta ja kattava raportointi
-  Asiakas voi ostaa myytävät tuotte itse mistä haluaa
-  Myös tuotetoimitukset lisäpalveluna
-  Yksi hinta, kaikki palvelut";
+$bulletList = "
+Pienikokoinen, 66x180x23 (LxKxS cm)
+Hyväksyy kaikki eurokolikot, 5snt - 2€
+Mobiili- ja luottokorttimaksu
+Helppo asennus ja käyttöönotto
+Vapaa hinnoittelu
+Etävalvonta ja kattava raportointi
+Asiakas voi ostaa myytävät tuotte itse mistä haluaa
+Myös tuotetoimitukset lisäpalveluna
+Yksi hinta, kaikki palvelut";
 
-  $shortDescriptionSubject = "Yrityksen lisämyynnin uusi ulottuvuus";
+$shortDescriptionSubject = "Yrityksen lisämyynnin uusi ulottuvuus";
 
-  $shortDescription = "
-  Olemme tuoneet markkinoille uuden pienikokoisen myyntiautomaatin
-  jolla jokainen yritys voi lisätä myyntiä helposti.
-  Varsinkin silloin kun henkilökuntaa ei ole saatavissa.
-  ";
+$shortDescription = "
+Olemme tuoneet markkinoille uuden pienikokoisen myyntiautomaatin
+jolla jokainen yritys voi lisätä myyntiä helposti.
+Varsinkin silloin kun henkilökuntaa ei ole saatavissa.
+";
 
-  $longContentSubject = "Vuokra-automaatti, miksi?";
+$longContentSubject = "Vuokra-automaatti, miksi?";
 
-  $longContent = "
-  Snacktime on tuonut markkinoille uuden mullistavan myyntiautomaatin. Automaatti on suunniteltu suoraan yrityksille,
-  joilla on halua lisätä myyntiä aikana jolloin se muutoin ei ole mahdollista. Lisämyynnin saaminen automaatin
-  kautta ei sido henkilökuntaa vaan vapauttaa sitä yrityksessä oleellisempana ja tuottavampaa työhön.
+$longContent = "
+Snacktime on tuonut markkinoille uuden mullistavan myyntiautomaatin. Automaatti on suunniteltu suoraan yrityksille,
+joilla on halua lisätä myyntiä aikana jolloin se muutoin ei ole mahdollista. Lisämyynnin saaminen automaatin
+kautta ei sido henkilökuntaa vaan vapauttaa sitä yrityksessä oleellisempana ja tuottavampaa työhön.
 
-  <br><br>
+<br><br>
 
-  Esim. Kuntosaleilla lisämyynnin saaminen kannattavasti silloin kun henkilökuntaa ei ole on ollut haastavaa.
-  Nyt tämä on voitu toteuttaa uuden mallisen myyntiautomaatin kanssa. Laite täytetään esim.
-  Aamulla kun henkilökunta saapuu työhön ja palveluja käyttävät asiakkaat voidaan ohjata myyjän sijaan
-  automaatille ostamaan haluamansa tuotteen. Myyjän aika vapautuu lisämyynnin tekemiseen ja jonotus vähenee.
+Esim. Kuntosaleilla lisämyynnin saaminen kannattavasti silloin kun henkilökuntaa ei ole on ollut haastavaa.
+Nyt tämä on voitu toteuttaa uuden mallisen myyntiautomaatin kanssa. Laite täytetään esim.
+Aamulla kun henkilökunta saapuu työhön ja palveluja käyttävät asiakkaat voidaan ohjata myyjän sijaan
+automaatille ostamaan haluamansa tuotteen. Myyjän aika vapautuu lisämyynnin tekemiseen ja jonotus vähenee.
 
-  <br><br>
+<br><br>
 
-  Yritys täyttää automaatin juuri niillä tuotteilla kuin haluaa ja saa tietenkin kaiken myyntitulon itselleen.
-  Yritys tekee vain yhden sopimuksen SnackTimen kanssa ja saa samalla mobiilimaksu ja luottokorttisopimukset.
+Yritys täyttää automaatin juuri niillä tuotteilla kuin haluaa ja saa tietenkin kaiken myyntitulon itselleen.
+Yritys tekee vain yhden sopimuksen SnackTimen kanssa ja saa samalla mobiilimaksu ja luottokorttisopimukset.
 
-  <br><br>
+<br><br>
 
-  Voimme tarjota yritykselle myös kattavan tuotetoimituspalvelun jolloin yritys tilaa laitteessa myytävät tuotteet meiltä,
-  tukkuhinnoin, jolloin edes uusien tuotteiden tilaamisen vaivaa ei ole.
-  Asennamme laitteen ja koulutamme henkilökunnan käyttämään sitä.
-  Vastaamme kaikesta laitteen toiminnasta pois lukien ilkivalta, jota vastaan meiltä saa myös vakuutuksen.
-  Ota rohkeasti yhteyttä niin kerromme lisää.
-  ";
-
-
-  $referenceTextList = "
-  Laitteen vuokraaminen on vapauttanut resursseja varsinaiseen myyntityöhön.
-  Vuokralaitepaikka, yritys x, Pertti Kettunen, yrittäjä.;
-  Asiakkaamme saavat lisäarvoa kun automaatti hoitaa iltamyynnin, joten jotain ostettavaa on aina tarjolla.
-  Vuokralaitepaikka, yritys y, Tiina Titityy, esimies.
-  ";
-
-  $referenceImageList = "
-  assets/images/placeHolder_2.JPG;
-  assets/images/placeHolder_2.JPG
-  ";
+Voimme tarjota yritykselle myös kattavan tuotetoimituspalvelun jolloin yritys tilaa laitteessa myytävät tuotteet meiltä,
+tukkuhinnoin, jolloin edes uusien tuotteiden tilaamisen vaivaa ei ole.
+Asennamme laitteen ja koulutamme henkilökunnan käyttämään sitä.
+Vastaamme kaikesta laitteen toiminnasta pois lukien ilkivalta, jota vastaan meiltä saa myös vakuutuksen.
+Ota rohkeasti yhteyttä niin kerromme lisää.
+";
 
 
-  $referenceTexts = explode(";\n", $referenceTextList);
-  $referenceImages = explode(";\n", $referenceImageList);
-  $images = explode("\n", $imageList);
-  $bullets = explode("\n", $bulletList);
+$referenceTextList = "
+Laitteen vuokraaminen on vapauttanut resursseja varsinaiseen myyntityöhön.
+Vuokralaitepaikka, yritys x, Pertti Kettunen, yrittäjä.;
+Asiakkaamme saavat lisäarvoa kun automaatti hoitaa iltamyynnin, joten jotain ostettavaa on aina tarjolla.
+Vuokralaitepaikka, yritys y, Tiina Titityy, esimies.
+";
+
+$referenceImageList = "
+assets/images/placeHolder_2.JPG;
+assets/images/placeHolder_2.JPG
+";
+
+
+$referenceTexts = explode(";\n", $referenceTextList);
+$referenceImages = explode(";\n", $referenceImageList);
+$images = explode("\n", $imageList);
+$bullets = explode("\n", $bulletList);
 
 ?>
 
@@ -87,13 +87,17 @@
 
     </div>
   </div>
-  
+
 
   <div class="contentElement" id="imagesElement">
-    <?php
+    <!-- <?php
       foreach ($images as $image)
         if($image != "") { echo "<img src='".$image."'>"; }
-     ?>
+     ?> -->
+ 
+    <?php   showImages($images); ?>
+
+
   </div>
 
 
